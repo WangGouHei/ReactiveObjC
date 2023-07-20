f18653180c2dc2df84e7faf3b20ac32232fced87
WangGouHei <w986151145@gmail.com>
2023-07-20 17:03:04 +0800

    swift PM

--- Package.swift
+++ Package.swift
@@ -1,42 +1,18 @@
-// swift-tools-version:5.3
-
+// swift-tools-version:5.1
 import PackageDescription
-
 let package = Package(
     name: "ReactiveObjC",
-    platforms: [
-        .iOS(.v8),
-        .macOS(.v10_10),
-        .watchOS(.v3),
-        .tvOS(.v9)
-    ],
     products: [
-            .library(
-                name: "ReactiveObjC",
-                targets: ["ReactiveObjC"]
-            )
-        ],
-        dependencies: [],
-        targets: [
-            .target(
-                name: "ReactiveObjC",
-                dependencies: [],
-                path: "ReactiveObjC",
-                exclude: [
-                    "ReactiveObjC/**/*{UIActionSheet,UIAlertView,UIBarButtonItem," +
-                    "UIButton,UICollectionReusableView,UIControl,UIDatePicker," +
-                    "UIGestureRecognizer,UIImagePicker,UIRefreshControl," +
-                    "UISegmentedControl,UISlider,UIStepper,UISwitch,UITableViewCell," +
-                    "UITableViewHeaderFooterView,UIText,MK}*"
-                ],
-                sources: [
-                    "ReactiveObjC/*.h",
-                    "ReactiveObjC/*.m",
-                    "ReactiveObjC/extobjc/*.h",
-                    "ReactiveObjC/extobjc/*.m",
-                ],
-                publicHeadersPath: "ReactiveObjC"
-            )
-        ],
-        swiftLanguageVersions: [.v5]
+        .library(name: "ReactiveObjC", targets: ["ReactiveObjC"])
+    ],
+    targets: [
+        .target(
+            name: "ReactiveObjC",
+            dependencies: [],
+            path: "ReactiveObjC",
+            exclude: ["Carthage","Documentation","Instruments"],
+            sources: ["*", "Deprecations+Removals.swift"],
+            publicHeadersPath: "include"
+        )
+    ]
 )
